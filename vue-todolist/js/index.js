34e541dbe9d093421f4dceb5901d2097d7c98b10
bi <2214871523@qq.com>
2020-02-14 17:32:31 +0800

    删除已勾选部分功能

--- vue-todolist/js/index.js
+++ vue-todolist/js/index.js
@@ -17,6 +17,15 @@ var vm = new Vue({
         deleteValue:function(item){
             var index = this.list.indexOf(item);
             this.list.splice(index,1);
+        },
+        clearCompleted:function(){
+            var newArr = this.list;
+            newArr.map(function(item,index){
+                if(item.checked){
+                    newArr.splice(index,1);
+                }
+            })
+            this.list=newArr
         }
     }
 })
